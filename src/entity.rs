80c7d039bfcffa6bffc8b6a4001d11fa88d96e99
jeffrey <jcouch210@gmail.com>
2025-03-24 15:45:40 -0800

    Replace match with if let in Entity::check_deletion

--- src/entity.rs
+++ src/entity.rs
@@ -140,13 +140,11 @@ impl Entity {
         let mut radius_squared = self.center.get_radius_squared();
         for i in (0..self.rings.len()).rev() {
             let ring = &mut self.rings[i];
-            match ring.get_full_radius_squared() {
-                Some(ring_radius) => {
-                    radius_squared = radius_squared.max(ring_radius);
-                }
-                None => {
-                    self.rings.swap_remove(i);
-                }
+
+            if let Some(ring_radius) = ring.get_full_radius_squared() {
+                radius_squared = radius_squared.max(ring_radius);
+            } else {
+                self.rings.swap_remove(i);
             }
         }
         self.radius = radius_squared.sqrt();
